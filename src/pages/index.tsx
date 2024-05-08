879b977db2b0d45a77ce8518bd54b95e4d8274ca
wuyifei.wufeifei <wuyifei.wufeifei@bytedance.com>
2024-05-08 10:55:05 +0800

    feat: 首屏麦芽糖音乐

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,76 @@
-import React from "react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
+import { TypeAnimation } from "react-type-animation";
 
-const Fullpage = () => (
-  <ReactFullpage
-    //fullpage options
-    licenseKey={"YOUR_KEY_HERE"}
-    credits={{}}
-    beforeLeave={(origin, destination, direction, trigger) => {
-      console.log(origin, destination, direction, trigger);
-    }}
-    scrollingSpeed={1000} /* Options here */
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
-);
+import cover from "@/resource/image/cover.png";
+import style from "./index.module.css";
+import deep_in_thought from "@/resource/image/deep_in_thought.png";
+import "animate.css";
+
+let audio: HTMLAudioElement;
+let hasClickPage0 = false;
+
+const Fullpage = () => {
+  const [current, setCurrent] = useState(0);
+  const [showCow, setCow] = useState(false);
+
+  useEffect(() => {
+    audio = new Audio("/music/maiyatang.ogg");
+    audio.volume = 1;
+    audio.preload = "auto";
+  }, []);
+
+  return (
+    <div className={style.page}>
+      <ReactFullpage
+        //fullpage options
+        licenseKey={"YOUR_KEY_HERE"}
+        credits={{}}
+        beforeLeave={(origin, destination, direction, trigger) => {
+          console.log(origin, destination, direction, trigger);
+        }}
+        scrollingSpeed={1000} /* Options here */
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div
+                className="section"
+                onClick={() => {
+                  if (!hasClickPage0) {
+                    audio.play();
+                    audio.onload = () => {
+                      audio.play();
+                    };
+                    hasClickPage0 = true;
+                  } else {
+                    setCow(true);
+                  }
+                }}
+              >
+                <Image className={style.cover} src={cover} alt="" />
+              </div>
+              <div className="section" style={{ backgroundColor: 'rgb(103, 193, 197)' }}>
+                <Image className={style.deep_in_thought} src={deep_in_thought} alt="" />
+                <TypeAnimation
+                  sequence={[
+                    '我在想',
+                    1000,
+                    '我在想, 未来的我们会怎么生活',
+                    500,
+                    '我在想, 未来的我们会怎么生活.又会拥有一个怎样的家呢'
+                  ]}
+                  wrapper="span"
+                  speed={50}
+                  style={{ fontSize: "2em", display: "block", fontFamily: 'MF YuanHei', position: 'absolute', bottom: '10px', left: '20px' }}
+                  repeat={0}
+                />
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    </div>
+  );
+};
 export default Fullpage;
